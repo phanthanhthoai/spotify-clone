56665d6c08c27a2fb3f382a81522fb75328e9d4c
phanthanhthoai <phanthanhthoai007@gmail.com>
2025-04-20 10:50:10 +0700

    Update controller

--- fe-client/src/components/SongController.jsx
+++ fe-client/src/components/SongController.jsx
@@ -1,6 +1,6 @@
 import SpotifyBackgroundIconButton from "./SpotifyBackgroundIconButton.jsx";
 import SpotifyIconButton from "./SpotifyIconButton.jsx";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useSelector} from "react-redux";
 
 export default function SongController() {
@@ -12,18 +12,23 @@ export default function SongController() {
         setIsPlaying(!isPlaying);
     }
 
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current?.play();
+            return;
+        }
+
+        audioRef.current?.pause();
+    }, [isPlaying]);
+
     return (
         <div className="flex gap-3 justify-center items-center">
             <audio ref={audioRef} src="../../src/assets/tháp_drill_tự_do_-_mck_prod_gaz.mp3"/>
-            {/*<audio controls>*/}
-            {/*    <source src="../../src/assets/tháp_drill_tự_do_-_mck_prod_gaz.mp3" type="audio/mpeg"/>*/}
-            {/*    Your browser does not support the audio element.*/}
-            {/*</audio>*/}
 
             <SpotifyIconButton name="SkipBack"/>
 
             <div onClick={toggle}>
-                <SpotifyBackgroundIconButton name={isPlaying ? 'Play' : 'Pause'}
+                <SpotifyBackgroundIconButton name={isPlaying ? 'Pause' : 'Play'}
                                              tooltip="Phát"></SpotifyBackgroundIconButton>
             </div>
 
