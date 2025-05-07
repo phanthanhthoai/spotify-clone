14f8070d9451f537c6205af11901b6be8c2f93a7
phanthanhthoai <phanthanhthoai007@gmail.com>
2025-05-08 03:08:06 +0700

    update

---
+++ fe-client/src/api/playlistService.js
@@ -0,0 +1,32 @@
+import axiosInstance from "./axiosInstance";
+
+const playlistService = {
+     getAllPlaylists: async () => {
+          const rawResponse = await axiosInstance.get("/playlist/?page=1&size=1000");
+          return rawResponse.data;
+     },
+     getPlaylistById: async (id) => {
+          const rawResponse = await axiosInstance.get(`/playlist/${id}`);
+          return rawResponse.data;
+     },
+     updatePlaylist: async (id, data) => {
+          const rawResponse = await axiosInstance.put(`/playlist/${id}`, data);
+          return rawResponse.data;
+     },
+     getPlaylistByUserId: async (userId) => {
+          const rawResponse = await axiosInstance.get(`/playlist/user/${userId}`);
+          return rawResponse.data;
+     },
+     
+     createPlaylist: async (playlistData) => {
+          const rawResponse = await axiosInstance.post("/playlists", playlistData);
+          return rawResponse.data;
+     },
+     
+     deletePlaylist: async (playlistId) => {
+          const rawResponse = await axiosInstance.delete(`/playlists/${playlistId}`);
+          return rawResponse.data;
+     },
+
+}
+export default playlistService;
